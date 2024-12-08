c0136259c954cc4fcd169eac1f1f2b662173b047
Silki-Ghoshikar <124167622+Silki-Ghoshikar@users.noreply.github.com>
2024-12-08 14:20:32 +0530

    Add files via upload

--- env/pkgIndex.tcl
+++ env/pkgIndex.tcl
@@ -1,9 +1,7 @@
 if {([info commands ::tcl::pkgconfig] eq "")
 	|| ([info sharedlibextension] ne ".dll")} return
 if {[::tcl::pkgconfig get debug]} {
-    package ifneeded registry 1.3.2 \
-            [list load [file join $dir tclreg13g.dll] registry]
+    package ifneeded dde 1.4.0 [list load [file join $dir tcldde14g.dll] dde]
 } else {
-    package ifneeded registry 1.3.2 \
-            [list load [file join $dir tclreg13.dll] registry]
+    package ifneeded dde 1.4.0 [list load [file join $dir tcldde14.dll] dde]
 }
